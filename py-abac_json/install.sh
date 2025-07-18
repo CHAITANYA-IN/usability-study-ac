11bec8f6628aad1ee8ecdfdb7a87b1a738128a7d
Chaitanya Bharat Shinge <43493174+CHAITANYA-IN@users.noreply.github.com>
2025-07-18 10:01:41 +0530

    Update install.sh

--- py-abac_json/install.sh
+++ py-abac_json/install.sh
@@ -2,5 +2,5 @@
 
 cd "$(dirname "$0")"
 
-python3 -m pip uninstall py_abac --break-system-packages
-python3 -m pip install ./py_abac-0.4.1-py3-none-any.whl --break-system-packages
+python3 -m pip uninstall py_abac
+python3 -m pip install ./py_abac-0.4.1-py3-none-any.whl
